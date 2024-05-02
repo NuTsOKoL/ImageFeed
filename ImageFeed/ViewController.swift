29d7873d04dcd3e36f3a04b5da87ba30f0fbd0ed
NuTsOKoL <mi.sei@yandex.ru>
2024-05-02 11:45:18 +0300

    8/19:8->2/5->4/4

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by mac on 02.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
-
 }
 
