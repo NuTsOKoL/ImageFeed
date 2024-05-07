5a7c69d4bfebc1268b19ae4fca41f43ad2fd68c0
NuTsOKoL <mi.sei@yandex.ru>
2024-05-07 11:28:39 +0300

    1

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,4 @@
+import UIKit
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 200
+        tableView.register(
+            ImagesListCell.self,
+            forCellReuseIdentifier: ImagesListCell.reuseIdentifier
+        )
+    }
+}
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell) { }
+}
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
