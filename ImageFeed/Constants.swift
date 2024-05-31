9ad99f436651d7d3619c486dda150d61803edaf4
NuTsOKoL <mi.sei@yandex.ru>
2024-05-31 06:11:24 +0300

    +constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+enum Constants {
+   static let accessKey = "@VXFSY5IC_I1qpNuOMAg03ZARKvKAw6yBnnJWQu8S7U"
+    static let secretKey = "KONSYkykFFdqsK2k8RJpGZSPth2aA0NG0shHnCiEF-I"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
