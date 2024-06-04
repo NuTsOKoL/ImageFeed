bc62850bdc1f44752731dba8bd453977f5fdf88b
NuTsOKoL <mi.sei@yandex.ru>
2024-06-04 06:55:58 +0300

    +

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+final class AuthViewController: UIViewController {
+    private let activeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Войти", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(activeButton)
+        setupConstraints()
+        configureAppearance()
+    }
+ 
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+                  activeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+                  activeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                  activeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -124),
+                  activeButton.heightAnchor.constraint(equalToConstant: 48)
+              ])
+      }
+    private func configureAppearance() {
+        
+    }
+}
